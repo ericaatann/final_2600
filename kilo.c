0e51036ddd7fc533f80c124896419f85bd84b071
ericaatann <ericatan@cpp.edu>
2021-12-04 06:05:30 +0000

    update

--- kilo.c
+++ kilo.c
@@ -191,13 +191,6 @@ void editorRefreshScreen() {
   abFree(&ab);
 }
 
-void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[H", 3);
-}
-
 /*** input ***/
 void editorMoveCursor(int key) {
   switch (key) {
